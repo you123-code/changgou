6c7e38eabc73f32fd3514b8a04d76303a046efac
游威 <1647373860@qq.com>
2021-11-26 15:10:48 +0800

    实现广告监控，存储到缓存

--- changgou-parent/changgou-service-api/changgou-service-content-api/src/main/java/com/changgou/content/feign/ContentFeign.java
+++ changgou-parent/changgou-service-api/changgou-service-content-api/src/main/java/com/changgou/content/feign/ContentFeign.java
@@ -1,9 +1,24 @@
 package com.changgou.content.feign;
 
+import com.changgou.content.pojo.Content;
+import entity.Result;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
 /**
  * @author youwei
  * @version 1.0
  * @date 2021/11/26 9:18
  */
+@FeignClient(name="content")
+@RequestMapping(value = "/content")
 public interface ContentFeign {
-}
+    /***
+     * 根据分类ID查询所有广告
+     */
+    @GetMapping(value = "/list/category")
+    Result<List<Content>> findByCategory(Long id);
+    }
--- changgou-parent/changgou-service/changgou-service-canal/src/main/java/com/changgou/CanalDataEventListener.java
+++ changgou-parent/changgou-service/changgou-service-canal/src/main/java/com/changgou/CanalDataEventListener.java
@@ -1,7 +1,16 @@
 package com.changgou;
 
+import com.alibaba.fastjson.JSON;
 import com.alibaba.otter.canal.protocol.CanalEntry;
-import com.xpand.starter.canal.annotation.*;
+import com.changgou.content.feign.ContentFeign;
+import com.changgou.content.pojo.Content;
+import com.xpand.starter.canal.annotation.CanalEventListener;
+import com.xpand.starter.canal.annotation.ListenPoint;
+import entity.Result;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+import java.util.List;
 
 /**
  * @author youwei
@@ -12,54 +21,47 @@ import com.xpand.starter.canal.annotation.*;
 //canal事务监听
 @CanalEventListener
 public class CanalDataEventListener {
-    /**
-     * 增加监听，只有增加之后的数据，
-     * @param eventType:当前操作的类型，增加数据
-     */
-    @InsertListenPoint
-    public  void onEventInsert(CanalEntry.EventType eventType, CanalEntry.RowData rowData){
-        for (CanalEntry.Column column : rowData.getAfterColumnsList()) {
-            System.out.println("列名：" + column.getName() + "-----------变更的数据："+column.getValue());
-        }
+ @Autowired
+    private ContentFeign contentFeign;
+ @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+ //自定义数据库操作进行监听
+    @ListenPoint(destination = "example",
+    schema = "changgou_content",
+    table = {"tb_content","tb_content_category"},
+    eventType = {
+            CanalEntry.EventType.UPDATE,
+            CanalEntry.EventType.DELETE,
+            CanalEntry.EventType.INSERT,
+    })
+    public void  onEvenCustomUpdate(CanalEntry.EventType eventType,CanalEntry.RowData rowData){
+        //获取列名为category_id的值
+         String categoryId = getColumnValue(eventType, rowData);
+        //2.调用feign 获取该分类下的所有的广告集合
+        Result<List<Content>> categoryResult =  contentFeign.findByCategory(Long.valueOf(categoryId));
+        List<Content> data = categoryResult.getData();
+        //使用redisTempl存储到redis中
+        stringRedisTemplate.boundValueOps("content_"  +  categoryId).set(JSON.toJSONString(data));
     }
-    /**
-     * 修改监听
-     */
-    @UpdateListenPoint
-    public  void onEventUpdate(CanalEntry.EventType eventType, CanalEntry.RowData rowData){
-        for (CanalEntry.Column column : rowData.getBeforeColumnsList()) {
-            System.out.println("修改前：列名：" + column.getName() + "-----------变更的数据："+column.getValue());
+    private String getColumnValue(CanalEntry.EventType eventType,CanalEntry.RowData rowData){
+        String categoryId = "";
+        //判断如果是删除则获取别额forList
+        if(eventType == CanalEntry.EventType.DELETE){
+            for (CanalEntry.Column column : rowData.getBeforeColumnsList()) {
+                if(column.getName().equalsIgnoreCase("category_id")){
+                    categoryId = column.getValue();
+                    return categoryId;
+                }
+            }
+        }else{
+            //判断如果是添加或者是更新，则获取afterList
+            for (CanalEntry.Column column : rowData.getAfterColumnsList()) {
+                if(column.getName().equalsIgnoreCase("category_id")){
+                    categoryId = column.getValue();
+                    return categoryId;
+                }
+            }
         }
-        for (CanalEntry.Column column : rowData.getAfterColumnsList()) {
-            System.out.println("修改后：列名：" + column.getName() + "-----------变更的数据："+column.getValue());
-        }
-    }
-
-    /**
-     * 删除监听
-     */
-    @DeleteListenPoint
-    public  void onEventDelete(CanalEntry.EventType eventType, CanalEntry.RowData rowData){
-        for (CanalEntry.Column column : rowData.getBeforeColumnsList()) {
-            System.out.println("删除前：列名：" + column.getName() + "-----------变更的数据："+column.getValue());
-        }
-    }
-
-    /**
-     * 自定义监听
-     */
-    @ListenPoint(eventType =  {
-            CanalEntry.EventType.DELETE, CanalEntry.EventType.UPDATE},
-            schema = {"changgou_content"},
-            table = {"tb_content"},
-            destination = "example"
-        )
-    public  void onEventCustomUpdate(CanalEntry.EventType eventType, CanalEntry.RowData rowData){
-        for (CanalEntry.Column column : rowData.getBeforeColumnsList()) {
-            System.out.println("自定义操作前：列名：" + column.getName() + "-----------变更的数据："+column.getValue());
-        }
-        for (CanalEntry.Column column : rowData.getAfterColumnsList()) {
-            System.out.println("自定义操作后：列名：" + column.getName() + "-----------变更的数据："+column.getValue());
-        }
+        return categoryId;
     }
 }
